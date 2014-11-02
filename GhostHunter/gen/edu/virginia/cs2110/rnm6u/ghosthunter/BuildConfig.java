c8b1e4324ee986bf61cbc99a846bf84cd70b1749
rmccampbell <rmccampbell7@gmail.com>
2014-11-02 16:07:58 -0500

    Made Android Project!!!

---
+++ GhostHunter/gen/edu/virginia/cs2110/rnm6u/ghosthunter/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.virginia.cs2110.rnm6u.ghosthunter;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
