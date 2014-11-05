b7d6268617a1aea90eb80fbc31c7580a2bb8a028
anthony2025 <anthony2025@gmail.com>
2014-11-05 04:44:00 -0500

    Splash screen added
    provitional splash screen used, there is a problem with the real one

---
+++ GhostHunter/src/edu/virginia/cs2110/rnm6u/ghosthunter/SplashScreen.java
@@ -0,0 +1,31 @@
+package edu.virginia.cs2110.rnm6u.ghosthunter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class SplashScreen extends Activity {
+
+	Button button;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// These two lines hide action bar and make activity full screen,
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// setContentView must be last to prevent crash
+		setContentView(R.layout.activity_splash_screen);
+	}
+
+	public void onTap(View view) {
+		Intent intent = new Intent(this, MainActivity.class);
+	    startActivity(intent);
+	}
+
+}
