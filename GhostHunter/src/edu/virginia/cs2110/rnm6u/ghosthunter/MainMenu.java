f4611c4a8150308783c7c6d2a252996be68d7544
anthony2025 <anthony2025@gmail.com>
2014-11-05 17:26:39 -0500

    Tiny change
    bye bye useless action bar

--- GhostHunter/src/edu/virginia/cs2110/rnm6u/ghosthunter/MainMenu.java
+++ GhostHunter/src/edu/virginia/cs2110/rnm6u/ghosthunter/MainMenu.java
@@ -2,8 +2,6 @@ package edu.virginia.cs2110.rnm6u.ghosthunter;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -20,22 +18,4 @@ public class MainMenu extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
