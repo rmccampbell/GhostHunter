20d84941b1f989c61edc3a2d110a9c4e650d8f7e
anthony2025 <anthony2025@gmail.com>
2014-11-05 17:08:26 -0500

    Tiny change
    -contdesc value in imagebutton

--- GhostHunter/gen/edu/virginia/cs2110/rnm6u/ghosthunter/R.java
+++ GhostHunter/gen/edu/virginia/cs2110/rnm6u/ghosthunter/R.java
@@ -23,7 +23,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int splashscreen2=0x7f020001;
+        public static final int splashscreen=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
@@ -39,6 +39,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int desc=0x7f050003;
         public static final int hello_world=0x7f050001;
     }
     public static final class style {
