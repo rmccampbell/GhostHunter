e627445a1f4c5754432cde99855b8efa8da2e6bc
rmccampbell <rmccampbell7@gmail.com>
2014-11-07 15:58:27 -0500

    Ignoring unnecessary files

--- GhostHunter/gen/edu/virginia/cs2110/rnm6u/ghosthunter/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.virginia.cs2110.rnm6u.ghosthunter;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
