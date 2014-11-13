8cb157a568b4ee2bd707f2bcb92d0941e45e08b5
rmccampbell <rmccampbell7@gmail.com>
2014-11-13 02:06:00 -0500

    Added UI and dummy movement system

--- GhostHunter/src/edu/virginia/cs2110/rnm6u/ghosthunter/GameActivity.java
+++ GhostHunter/src/edu/virginia/cs2110/rnm6u/ghosthunter/GameActivity.java
@@ -2,12 +2,17 @@ package edu.virginia.cs2110.rnm6u.ghosthunter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.RelativeLayout;
 
-public class GameActivity extends Activity {
+public class GameActivity extends Activity implements OnTouchListener, OnClickListener {
 
-	GameView v;
+	GameView game;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,20 +22,77 @@ public class GameActivity extends Activity {
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-		v = new GameView(this);
-		setContentView(v);
+		setContentView(R.layout.game_ui);
+
+		game = new GameView(this);
+		RelativeLayout layout = (RelativeLayout) findViewById(R.id.game_layout);
+		layout.addView(game, 0);
+
+		findViewById(R.id.button_down).setOnTouchListener(this);
+		findViewById(R.id.button_up).setOnTouchListener(this);
+		findViewById(R.id.button_right).setOnTouchListener(this);
+		findViewById(R.id.button_left).setOnTouchListener(this);
+		findViewById(R.id.button_attack).setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			switch (v.getId()) {
+			case R.id.button_down:
+				game.moveDown();
+				break;
+			case R.id.button_up:
+				game.moveUp();
+				break;
+			case R.id.button_right:
+				game.moveRight();
+				break;
+			case R.id.button_left:
+				game.moveLeft();
+				break;
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+			switch (v.getId()) {
+			case R.id.button_down:
+				game.stopDown();
+				break;
+			case R.id.button_up:
+				game.stopUp();
+				break;
+			case R.id.button_right:
+				game.stopRight();
+				break;
+			case R.id.button_left:
+				game.stopLeft();
+				break;
+			}
+			break;
+		}
+		return false;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_attack:
+			game.attack();
+			break;
+		}
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		v.pause();
+		game.pause();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		v.resume();
+		game.resume();
 	}
 
 }
--- GhostHunter/src/edu/virginia/cs2110/rnm6u/ghosthunter/GameView.java
+++ GhostHunter/src/edu/virginia/cs2110/rnm6u/ghosthunter/GameView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -13,37 +14,115 @@ public class GameView extends SurfaceView implements Runnable, SurfaceHolder.Cal
 	Thread thread = null;
 	SurfaceHolder holder;
 	boolean running = false;
-	int x = 80;
+
+	int x = 80, y = 80;
+	int xSpeed = 0, ySpeed = 0;
 
 	public GameView(Context context) {
 		super(context);
 		holder = getHolder();
+		holder.addCallback(this);
 	}
 
 	@Override
 	public void run() {
 		while (running) {
-			if (!holder.getSurface().isValid()) {
-				continue;
-			}
+			update();
 			Canvas c = holder.lockCanvas();
-			drawTo(c);
+			drawGame(c);
 			holder.unlockCanvasAndPost(c);
-			x+=6;
-			x %= 1200;
 		}
 	}
-	
-	public void drawTo(Canvas c) {
+
+	private void update() {
+		x += xSpeed;
+		y += ySpeed;
+	}
+
+	public void drawGame(Canvas c) {
 		c.drawRGB(255, 255, 255);
 		Paint p = new Paint();
+		p.setStyle(Style.FILL);
+		p.setColor(Color.LTGRAY);
+		c.drawCircle(x, y, 80, p);
 		p.setStyle(Style.STROKE);
 		p.setStrokeWidth(8);
 		p.setColor(Color.BLUE);
-		c.drawCircle(x, 350, 80, p);
+		c.drawCircle(x, y, 80, p);
+	}
+
+	public void moveUp() {
+		Log.d("GameView", "move up");
+		ySpeed = -8;
+	}
+	
+	public void moveDown() {
+		Log.d("GameView", "move down");
+		ySpeed = 8;
+	}
+	
+	public void moveLeft() {
+		Log.d("GameView", "move left");
+		xSpeed = -8;
+	}
+	
+	public void moveRight() {
+		Log.d("GameView", "move right");
+		xSpeed = 8;
+	}
+	
+	public void stopUp() {
+		Log.d("GameView", "stop up");
+		ySpeed = 0;
+	}
+	
+	public void stopDown() {
+		Log.d("GameView", "stop down");
+		ySpeed = 0;
+	}
+	
+	public void stopLeft() {
+		Log.d("GameView", "stop left");
+		xSpeed = 0;
+	}
+	
+	public void stopRight() {
+		Log.d("GameView", "stop right");
+		xSpeed = 0;
+	}
+	
+	public void attack() {
+		Log.d("GameView", "attack");
+
+	}
+	
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		Log.d("GameView", "surface created");
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		Log.d("GameView", "surface destroyed");
+		if (running) {
+			pause();
+		}
+	}
+
+	public void resume() {
+		Log.d("GameView", "resumed");
 	}
 
 	public void pause() {
+		Log.d("GameView", "paused");
 		running = false;
 		while (true) {
 			try {
@@ -54,31 +133,6 @@ public class GameView extends SurfaceView implements Runnable, SurfaceHolder.Cal
 			}
 		}
 		thread = null;
-	}
-
-	public void resume() {
-		running = true;
-		thread = new Thread(this);
-		thread.start();
-	}
-
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
