cd726f5f92087f57eecea49fb8184b8ee3b39e30
rmccampbell <rmccampbell7@gmail.com>
2014-11-29 15:34:32 -0500

    small fix

--- GhostHunter/src/edu/virginia/cs2110/rnm6u/ghosthunter/GameOver.java
+++ GhostHunter/src/edu/virginia/cs2110/rnm6u/ghosthunter/GameOver.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -24,8 +25,16 @@ public class GameOver extends Activity {
 		setContentView(R.layout.game_over);
 	}
 
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			onTap(null);
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
 	public void onTap(View view) {
-		Log.d(TAG, "Tapped");
 		Intent intent = new Intent(this, MainMenu.class);
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		startActivity(intent);
